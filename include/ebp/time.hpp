9503315b0422e557ecf27c8e4354eba29f204183
movsb <chkesp@gmail.com>
2023-05-10 04:23:06 +0800

    更多时间处理函数

--- include/ebp/time.hpp
+++ include/ebp/time.hpp
@@ -5,37 +5,38 @@
 
 #include "base.hpp"
 
-#include "esp_etm.h"
-#include "esp_timer.h"
+#include <esp_etm.h>
+#include <esp_timer.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
 namespace ebp {
 
 namespace time {
-	
+
 class Duration {
 public:
 	Duration(int64_t nanoseconds) : _t(nanoseconds) {}
 
-	int64_t nanoseconds()   const { return _t;                      }
-	int64_t microseconds()  const { return nanoseconds()    / 1000; }
-	int64_t milliseconds()  const { return microseconds()   / 1000; }
-	int64_t seconds()       const { return milliseconds()   / 1000; }
-	int64_t minutes()       const { return seconds()        / 60;   }
-	int64_t hours()         const { return minutes()        / 60;   }
-	int64_t days()          const { return hours()          / 24;   }
+	int64_t nanoseconds()   const { return _t;                          }
+	int64_t microseconds()  const { return nanoseconds()    / 1000;     }
+	int64_t milliseconds()  const { return microseconds()   / 1000;     }
+	double  seconds()       const { return milliseconds()   / 1000.0;   }
+	double  minutes()       const { return seconds()        / 60.0;     }
+	double  hours()         const { return minutes()        / 60.0;     }
+	double  days()          const { return hours()          / 24.0;     }
 	
 	Duration operator*(int64_t n)       const { return _t * n;          }
 	Duration operator/(int64_t n)       const { return _t / n;          }
-	
+
 protected:
 	int64_t _t;
 };
 	
-Duration operator*(int64_t n, const Duration &duration) { return duration * n; }
-Duration operator/(int64_t n, const Duration &duration) { return duration / n; }
-	
+// To Allow expressions like N * Duration, where N is a constant.
+inline Duration operator*(int64_t n, const Duration &duration) { return duration * n; }
+inline Duration operator/(int64_t n, const Duration &duration) { return duration / n; }
+
 static const Duration
 	Nanosecond      = { 1 },
 	Microsecond     = { Nanosecond    * 1000 },
@@ -50,7 +51,40 @@ static void sleep(Duration duration) {
 	vTaskDelay(duration.milliseconds() / portTICK_PERIOD_MS);
 }
 
+enum class Month : uint8_t {
+	January     = 1,
+	February    = 2,
+	March       = 3,
+	April       = 4,
+	May         = 5,
+	June        = 6,
+	July        = 7,
+	August      = 8,
+	September   = 9,
+	October     = 10,
+	November    = 11,
+	December    = 12,
+};
+
+enum class Weekday : uint8_t {
+	Sunday      = 0,
+	Monday      = 1,
+	Tuesday     = 2,
+	Wednesday   = 3,
+	Thursday    = 4,
+	Friday      = 5,
+	Saturday    = 6,
+};
+
 class Time {
+public:
+	Time() {
+		_sec    = 0;
+		_micro  = 0;
+		_mono   = 0;
+		_offset = 0;
+	}
+
 private:
 	int64_t     _sec        :33;    // s, 272 years, from 1970-01-01 00:00:00.
 	int64_t     _micro      :20;    // us, 0-999999us
@@ -58,10 +92,39 @@ private:
 	int64_t     _offset     :7;     // 15m, the number 15 minutes, ± 14 hours.
 
 public:
-	Duration operator-(const Time& other) {
-		assert(_mono > 0 && other._mono > 0);
+	bool empty() const { return _sec == 0 && _micro == 0 && _mono == 0; }
+	Time operator+(const Duration &d) const;
+	Duration operator-(const Time& other) const {
+		assert(hasMono() && other.hasMono());
 		return (_mono - other._mono) * Microsecond;
 	}
+	bool operator>(const Time &other) const {
+		if (hasMono() && other.hasMono()) { return _mono > other._mono; }
+		return (_sec > other._sec) || (_sec == other._sec && _micro > other._micro);
+	}
+	bool operator<(const Time &other) const {
+		if (hasMono() && other.hasMono()) { return _mono < other._mono; }
+		return (_sec < other._sec) || (_sec == other._sec && _micro < other._micro);
+	}
+	bool operator==(const Time &other) const {
+		if (hasMono() && other.hasMono()) { return _mono == other._mono; }
+		return _sec == other._sec && _micro == other._micro;
+	}
+	
+	std::tuple<int, Month, int> date() const {
+		auto [year, month, day, _] = absDate(abs());
+		return { year, month, day };
+	}
+
+	int     year()          const { return std::get<0>(absDate(abs())); }
+	Month   month()         const { return std::get<1>(absDate(abs())); }
+	int     day()           const { return std::get<2>(absDate(abs())); }
+	int     yearDay()       const { return std::get<3>(absDate(abs())); }
+	int     hour()          const { return int(abs() % (24*60*60)) / (60*60);   }
+	int     minute()        const { return int(abs() % (60*60)) / (60);         }
+	int     second()        const { return int(abs() % 60);                     }
+	int     microsecond()   const { return _micro; }
+	int     nanosecond()    const { return 0;      }
 	
 	std::string toString() const {
 		auto t = _sec * 1000000 + _micro;
@@ -72,21 +135,19 @@ public:
 	
 private:
 	friend Time now();
+
+	bool hasMono() const { return _mono > 0; }
+	void stripMono() { _mono = 0; }
+	// abs returns the time t as an absolute time, adjusted by the zone offset.
+	// It is called when computing a presentation property like Month or Hour.
+	uint64_t abs() const { return _sec + _offset * 15 * 60; }
+	// int *year, Month *month, int *day, int *yday
+	std::tuple<int, Month, int, int> absDate(uint64_t abs) const;
 };
 
-Time now() {
-	timeval val{0, 0};
-	if (gettimeofday(&val, NULL) != 0) {
-		abort();
-	}
-	
-	Time t = {};
-	t._sec      = val.tv_sec;
-	t._micro    = val.tv_usec;
-	t._offset   = 0;
-	t._mono     = esp_timer_get_time();
-	return t;
-}
+Time now();
+inline Duration since(const Time& past)    { return now() - past;      }
+inline Duration until(const Time& future)  { return future - now();    }
 
 } // namespace time
 
---
+++ time.cpp
@@ -0,0 +1,145 @@
+#include "ebp/time.hpp"
+
+namespace ebp {
+namespace time {
+
+Time  Time::operator+(const Duration &d) const {
+	Time t(*this);
+	
+	int64_t dSec = d.microseconds() / int64_t(1e6);
+	int64_t mSec = t._micro + d.microseconds() % int64_t(1e6);
+	
+	if (mSec >= 1e6) {
+		dSec++;
+		mSec -= 1e6;
+	} else if (mSec < 0) {
+		dSec--;
+		mSec += 1e6;
+	}
+
+	t._micro = mSec;
+	t._sec += dSec;
+
+	t._mono += d.microseconds();
+
+	return t;
+}
+
+namespace {
+	constexpr int secondsPerMinute  = 60;
+	constexpr int secondsPerHour    = 60*60;
+	constexpr int secondsPerDay     = 24*60*60;
+	constexpr int daysPerWeek       = 7 * secondsPerDay;
+	constexpr int daysPer400Years   = 365 * 400 + 97;
+	constexpr int daysPer100Years   = 365 * 100 + 24;
+	constexpr int daysPer4Years     = 365 * 4   +  1;
+
+	static bool isLeapYear(int year) {
+		return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+	}
+
+
+	// daysBefore[m] counts the number of days in a non-leap year
+	// before month m begins. There is an entry for m=12, counting
+	// the number of days before January of next year (365).
+	uint32_t daysBefore[] = {
+		0,
+		31,
+		31 + 28,
+		31 + 28 + 31,
+		31 + 28 + 31 + 30,
+		31 + 28 + 31 + 30 + 31,
+		31 + 28 + 31 + 30 + 31 + 30,
+		31 + 28 + 31 + 30 + 31 + 30 + 31,
+		31 + 28 + 31 + 30 + 31 + 30 + 31 + 31,
+		31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30,
+		31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31,
+		31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30,
+		31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31,
+	};
+}
+
+std::tuple<int, Month, int, int> Time::absDate(uint64_t absolute) const
+{
+	// Split into time and day.
+	uint64_t d = absolute / secondsPerDay;
+	
+	// Account for 400 year cycles.
+	uint64_t n = d / daysPer400Years;
+	uint64_t y = 400 * n;
+	d -= daysPer400Years * n;
+
+	// Cut off 100-year cycles.
+	// The last cycle has one extra leap year, so on the last day
+	// of that year, day / daysPer100Years will be 4 instead of 3.
+	// Cut it back down to 3 by subtracting n>>2.
+	n = d / daysPer100Years;
+	n -= n >> 2;
+	y += 100 * n;
+	d -= daysPer100Years * n;
+
+	// Cut off 4-year cycles.
+	// The last cycle has a missing leap year, which does not
+	// affect the computation.
+	n = d / daysPer4Years;
+	y += 4 * n;
+	d -= daysPer4Years * n;
+	
+	// Cut off years within a 4-year cycle.
+	// The last year is a leap year, so on the last day of that year,
+	// day / 365 will be 4 instead of 3. Cut it back down to 3
+	// by subtracting n>>2.
+	n = d / 365;
+	n -= n >> 2;
+	y += n;
+	d -= 365 * n;
+
+	int year = int(int64_t(y) + 1970);
+	int yday = int(d);
+	
+	Month month;
+	
+	int day = int(d);
+	if (isLeapYear(year)) {
+		if (day > 31 + 29 - 1) {
+			day--;
+		} else if (day == 31 + 29 -1) {
+			month = Month::February;
+			day = 29;
+			return { year, month, day, yday };
+		}
+	}
+	
+	month = Month(day / 31);
+	int end = int(daysBefore[int(month)+1]);
+	int begin;
+	if (day >= end) {
+		++*reinterpret_cast<std::underlying_type<Month>::type*>(&month);
+		begin = end;
+	} else {
+		begin = int(daysBefore[int(month)]);
+	}
+	
+	++*reinterpret_cast<std::underlying_type<Month>::type*>(&month);
+	day = day - begin + 1;
+	
+	return { year, month, day, yday };
+}
+
+Time now()
+{
+	timeval val{0, 0};
+	if (gettimeofday(&val, NULL) != 0) {
+		abort();
+	}
+	
+	Time t = {};
+	t._sec      = val.tv_sec;
+	t._micro    = val.tv_usec;
+	t._offset   = 0;
+	t._mono     = esp_timer_get_time();
+	return t;
+}
+
+}
+}
