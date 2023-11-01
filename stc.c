2bbb69b7e14a97a8afea8e40ed7842a171196967
movsb <chkesp@gmail.com>
2023-11-01 15:15:32 +0800

    first commit

---
+++ stc.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+
+#define FOSC 11059200UL
+#include "stc.h"
+
+volatile int8 uartBusy;
+
+void UARTInit(uint32 baudrate) {
+	uint16 t = 65536 - FOSC / baudrate / 4;
+	SCON = 0x50;
+	T2L = (byte)(t);
+	T2H = (byte)(t >> 8);
+	AUXR = 0x15;
+	ES = 1;
+	uartBusy = 0;
+}
+
+void UARTSendByte(uint8 c) {
+	while(uartBusy);
+	uartBusy = 1;
+	SBUF = c;
+}
+
+void UARTSendData(const uint8 *data, uint8 len) {
+	while(len-- > 0) {
+		UARTSendByte(*data);
+		++data;
+	}
+}
+
+void UARTSendString(const int8 *str) {
+	while(*str) {
+		UARTSendByte(*str);
+		++str;
+	}
+}
+
+static void _UartOutputChar(char c, void *p) {
+	(p);
+	UARTSendByte(c);
+}
+
+void UARTSendFormat(const int8 *format, ...) {
+	va_list ap;
+	va_start(ap, format);
+	_print_format(_UartOutputChar, NULL, format, ap);
+	va_end(ap);
+}
+
+void UARTHandler(void) INTERRUPT(4) {
+	if(TI) {
+		TI = 0;
+		uartBusy = 0;
+	}
+	if (RI) {
+		RI = 0;
+		// read SBUF;
+	}
+}
+
+#define CLK(n) (P32 = (n))
+#define DAT(n) (P33 = (n))
+#define DAT_GET() (P33)
+#define CSN(n)  (P54 = (n))
+
+static void _SpiWriteByte(uint8 c);
+static uint8 _SpiReadByte(void);
+
+// SPI 函数
+void SpiInit(void) {
+
+}
+
+static void _SpiWriteByte(uint8 c) {
+	CLK(0);
+	
+	for(int8 i=0; i<8; i++) {
+		CLK(0);
+		DAT(c & 0x80 ? 1 : 0);
+		CLK(1);
+		c <<= 1;
+	}
+
+	CLK(0);
+}
+
+static uint8 _SpiReadByte(void) {
+	uint8 d = 0;
+	CLK(0);
+	for(int8 i=0; i<8; i++) {
+		d <<= 1;
+		CLK(1);
+		if (DAT_GET() > 0) {
+			d |= 1;
+			UARTSendFormat("读取到高电平！\r\n");
+		} else {
+			// UARTSendFormat("读取到低电平！\r\n");
+		}
+		CLK(0);
+	}
+	CLK(0);
+	return d;
+}
+
+void SpiWrite(uint8 cmd, uint8 value) {
+	CSN(0);
+	_SpiWriteByte(cmd);
+	_SpiWriteByte(value);
+	CSN(1);
+}
+
+void SpiWrites(uint8 cmd, const uint8 *data, uint8 len) {
+	CSN(0);
+	_SpiWriteByte(cmd);
+	while(len > 0) {
+		_SpiWriteByte(*data);
+		++data;
+		--len;
+	}
+	CSN(1);
+}
+
+uint8 SpiRead(uint8 cmd) {
+	uint8 value;
+	CSN(0);
+	_SpiWriteByte(cmd);
+	// CSN(1);
+	// CSN(0); // "并且 CSN 拉低后，DATA 切换成输出口"
+	value = _SpiReadByte();
+	CSN(1);
+	// 写之前会拉低，这儿没必要拉
+	// CSN(0); // "当CSN 再次拉低之后，DATA 切换为输入口"
+	return value;
+}
+
+void SpiReads(uint8 cmd, uint8 *data, uint8 len) {
+	CSN(0);
+	_SpiWriteByte(cmd);
+	// CSN(1);
+	// CSN(0);
+	while(len > 0) {
+		*data = _SpiReadByte();
+		++data;
+		--len;
+	}
+	CSN(1);
+}
---
+++ stc.h
@@ -0,0 +1,88 @@
+#ifndef __STC_H__
+#define __STC_H__
+
+#ifndef __SDCC
+#define __SDCC
+#endif
+
+#ifndef FOSC
+	#error "FOSC is not defined"
+#endif
+
+// 一些基础类型
+// TODO：类型大小检测符合位数。
+typedef signed char         int8;
+typedef signed int          int16;
+typedef signed long         int32;
+
+typedef unsigned char       byte;
+typedef unsigned char       uint8;
+typedef unsigned int        uint16;
+typedef unsigned long       uint32;
+
+// 适用于 SDCC - Small Device C Compiler 的宏定义
+#if defined (SDCC) || defined (__SDCC)
+
+#define SBIT(name, addr, bit)       __sbit  __at(addr+bit)  name
+#define SFR(name, addr)             __sfr   __at(addr)      name
+#define INTERRUPT(vector)           __interrupt (vector)
+
+#endif // SDCC
+
+// 特殊功能寄存器
+SFR(AUXR,   0x8E);
+SFR(SCON,   0x98);
+	SBIT(RI,    0x98, 0);
+	SBIT(TI,    0x98, 1);
+SFR(SBUF,   0x99);
+
+SFR(IE,     0xA8);
+	SBIT(ES,    0xA8,   4);
+	SBIT(EA,    0xA8,   7);
+	
+SFR(P3,     0xB0);
+	SBIT(P30,   0xB0,   0);
+	SBIT(P31,   0xB0,   1);
+	SBIT(P32,   0xB0,   2);
+	SBIT(P33,   0xB0,   3);
+	
+SFR(P5,     0xC8);
+	SBIT(P54,   0xC8,   4);
+
+SFR(P3M1,   0xB1);
+SFR(P3M0,   0xB2);
+SFR(AUX,    0x8E);
+SFR(P5M1,   0xC9);
+SFR(P5M0,   0xCA);
+SFR(T2H,    0xD6);
+SFR(T2L,    0xD7);
+
+// 全局中断相关函数
+inline void EnableInterrupts(void) {
+	EA = 1;
+}
+
+inline void DisableInterrupts(void) {
+	EA = 0;
+}
+
+void UARTInit(uint32 baudrate);
+void UARTSendByte(uint8 byte);
+void UARTSendData(const uint8 *data, uint8 len);
+void UARTSendString(const int8 *str);
+void UARTSendFormat(const int8 *format, ...);
+
+// 中断函数必须要在头文件中声明，否则中断函数不会被执行
+// https://blog.csdn.net/openblog/article/details/72942315
+void UARTHandler(void) INTERRUPT(4);
+
+// SPI
+// 命令和寄存器并不一样，SPI 第一个字节是命令，而命令分读、写寄存器、清空发送数据等。
+// 所以不要认为 spi 读写的都是寄存器。
+void    SpiInit(void);
+void    SpiWrite        (uint8 cmd, uint8 value);
+void    SpiWrites       (uint8 cmd, const uint8 *data, uint8 len);
+uint8   SpiRead         (uint8 cmd);
+void    SpiReads        (uint8 cmd, uint8 *data, uint8 len);
+
+#endif // __STC_H__
