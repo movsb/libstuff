a91602ec54b1a3ccaeb98fcb9153a03295a81156
movsb <chkesp@gmail.com>
2023-12-11 05:52:01 +0800

    simplify debug and main initialization

--- ports/ch32v00x/include/ch32v00x/debug.h
+++ ports/ch32v00x/include/ch32v00x/debug.h
@@ -1,52 +1,27 @@
-/********************************** (C) COPYRIGHT  *******************************
- * File Name          : debug.h
- * Author             : WCH
- * Version            : V1.0.0
- * Date               : 2022/08/08
- * Description        : This file contains all the functions prototypes for UART
- *                      Printf , Delay functions.
- *********************************************************************************
- * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
- * Attention: This software (modified or not) and binary are used for 
- * microcontroller manufactured by Nanjing Qinheng Microelectronics.
- *******************************************************************************/
-#ifndef __DEBUG_H
-#define __DEBUG_H
+#pragma once
 
 #ifdef __cplusplus
  extern "C" {
 #endif
 
-#include <ch32v00x/ch32v00x.h>
 #include <stdio.h>
-
-/* UART Printf Definition */
-#define DEBUG_UART1_NoRemap   1  //Tx-PD5
-#define DEBUG_UART1_Remap1    2  //Tx-PD0
-#define DEBUG_UART1_Remap2    3  //Tx-PD6
-#define DEBUG_UART1_Remap3    4  //Tx-PC0
-
-/* DEBUG UATR Definition */
-#ifndef DEBUG
-#define DEBUG   DEBUG_UART1_NoRemap
-#endif
-
-/* SDI Printf Definition */
-#define SDI_PR_CLOSE   0
-#define SDI_PR_OPEN    1
-
-#ifndef SDI_PRINT
-#define SDI_PRINT   SDI_PR_CLOSE
-#endif
+#include <ch32v00x/ch32v00x.h>
 
 void Delay_Init(void);
 void Delay_Us(uint32_t n);
 void Delay_Ms(uint32_t n);
+
+/**
+ * @brief 把 printf 绑定到串口上。
+ * 
+ * 端口号为：PD6，重映射为 UTX_2.
+ * 
+ * 不支持 SDIO  printf，那样会把 SDIO 占用，导致无法烧录程序。
+ * 
+ * @param baudrate  波特率
+*/
 void USART_Printf_Init(uint32_t baudrate);
-void SDI_Printf_Enable(void);
 
 #ifdef __cplusplus
 }
 #endif
-
-#endif /* __DEBUG_H */
--- ports/ch32v00x/main.c
+++ ports/ch32v00x/main.c
@@ -3,50 +3,31 @@
 
 int main(void)
 {
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-    SystemCoreClockUpdate();
-    Delay_Init();
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	SystemCoreClockUpdate();
+	Delay_Init();
+	USART_Printf_Init(115200);
 
-    GPIO_InitTypeDef g = {0};
-    g.GPIO_Pin = GPIO_Pin_1;
-    g.GPIO_Mode = GPIO_Mode_Out_OD;
-    g.GPIO_Speed = GPIO_Speed_2MHz;
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-    GPIO_Init(GPIOC, &g);
-    
-    g.GPIO_Pin = GPIO_Pin_6;
-    g.GPIO_Mode = GPIO_Mode_AF_PP;
-    g.GPIO_Speed = GPIO_Speed_50MHz;
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_USART1, ENABLE);
-    GPIO_Init(GPIOD, &g);
-    GPIO_PinRemapConfig(GPIO_PartialRemap2_USART1, ENABLE);
+	printf("SystemClk:%lu\r\n",SystemCoreClock);
+	printf( "ChipID:%08lx\r\n", DBGMCU_GetCHIPID() );
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	GPIO_InitTypeDef g = {
+		.GPIO_Pin   = GPIO_Pin_1,
+		.GPIO_Speed = GPIO_Speed_10MHz,
+		.GPIO_Mode  = GPIO_Mode_Out_OD,
+	};
+	GPIO_Init(GPIOC, &g);
 
-    USART_InitTypeDef USART_InitStructure = {0};
-    USART_InitStructure.USART_BaudRate = 115200;
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-    USART_InitStructure.USART_Parity = USART_Parity_No;
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_InitStructure.USART_Mode = USART_Mode_Tx;
+	sum(1, 2);
 
-    USART_Init(USART1, &USART_InitStructure);
-    USART_Cmd(USART1, ENABLE);
-
-    printf("SystemClk:%lu\r\n",SystemCoreClock);
-    printf( "ChipID:%08lx\r\n", DBGMCU_GetCHIPID() );
-
-
-    int i = 0;
-    
-    sum(1, 2);
-
-    while (1)
-    {
-        Delay_Ms(100);
-        GPIO_WriteBit(GPIOC,  GPIO_Pin_1, Bit_RESET);
-        Delay_Ms(100);
-        GPIO_WriteBit(GPIOC,  GPIO_Pin_1, Bit_SET);
-        
-        printf("count: %d\r\n", ++i);
-    }
+	for (int i = 1;; i++)
+	{
+		Delay_Ms(100);
+		GPIO_WriteBit(GPIOC,  GPIO_Pin_1, Bit_RESET);
+		Delay_Ms(100);
+		GPIO_WriteBit(GPIOC,  GPIO_Pin_1, Bit_SET);
+		
+		printf("count: %d\r\n", i);
+	}
 }
