8f16bae5277d35211581c8738b6888b9b9d482bc
movsb <chkesp@gmail.com>
2023-12-12 07:27:01 +0800

    删除没存在感/没意义的函数
    - 可以用 us * 1000 实现
    - 节约 2 字节 .data
    - 节约 120 字节 .text

--- ports/ch32v00x/stub/debug.c
+++ ports/ch32v00x/stub/debug.c
@@ -2,7 +2,6 @@
 #include <ch32v00x/debug.h>
 
 static uint8_t  p_us = 0;
-static uint16_t p_ms = 0;
 
 /*********************************************************************
  * @fn      Delay_Init
@@ -14,7 +13,6 @@ static uint16_t p_ms = 0;
 void Delay_Init(void)
 {
     p_us = SystemCoreClock / 8000000;
-    p_ms = (uint16_t)p_us * 1000;
 }
 
 /*********************************************************************
@@ -32,30 +30,6 @@ void Delay_Us(uint32_t n)
 
     SysTick->SR &= ~(1 << 0);
     i = (uint32_t)n * p_us;
-
-    SysTick->CMP = i;
-    SysTick->CNT = 0;
-    SysTick->CTLR |=(1 << 0);
-
-    while((SysTick->SR & (1 << 0)) != (1 << 0));
-    SysTick->CTLR &= ~(1 << 0);
-}
-
-/*********************************************************************
- * @fn      Delay_Ms
- *
- * @brief   Millisecond Delay Time.
- *
- * @param   n - Millisecond number.
- *
- * @return  None
- */
-void Delay_Ms(uint32_t n)
-{
-    uint32_t i;
-
-    SysTick->SR &= ~(1 << 0);
-    i = (uint32_t)n * p_ms;
 
     SysTick->CMP = i;
     SysTick->CNT = 0;
