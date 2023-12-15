d638494fd67fa559320eb8812665a886f5c26aae
movsb <chkesp@gmail.com>
2023-12-15 20:03:39 +0800

    不依赖上级目录结构

--- ports/esp32/include.cmake
+++ ports/esp32/include.cmake
@@ -1,4 +1,6 @@
 # 注意这里是 PUBLIC，由依赖组件自己实现的，否则会链接失败
-target_sources(${COMPONENT_LIB} PUBLIC ${CMAKE_SOURCE_DIR}/_stuff/ports/esp32/stuff.c)
-target_include_directories(${COMPONENT_LIB} PRIVATE ${CMAKE_SOURCE_DIR}/_stuff/base/include)
+file(GLOB SRCS ${CMAKE_CURRENT_LIST_DIR}/*.c ${CMAKE_CURRENT_LIST_DIR}/*.cpp)
+set(STUFF_PATH ${CMAKE_CURRENT_LIST_DIR}/../..)
+target_sources(${COMPONENT_LIB} PUBLIC ${STUFF_PATH}/ports/esp32/stuff.c)
+target_include_directories(${COMPONENT_LIB} PRIVATE ${STUFF_PATH}/base/include)
 target_link_libraries(${COMPONENT_LIB} PRIVATE base)
