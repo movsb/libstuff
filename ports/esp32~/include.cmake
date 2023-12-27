4e37cc2078e0b2396c32d56f0f3e08dc7da2e132
movsb <chkesp@gmail.com>
2023-12-28 03:32:21 +0800

    remove

--- ports/esp32~/include.cmake
+++
@@ -1,6 +0,0 @@
-# 注意这里是 PUBLIC，由依赖组件自己实现的，否则会链接失败
-file(GLOB SRCS ${CMAKE_CURRENT_LIST_DIR}/*.c ${CMAKE_CURRENT_LIST_DIR}/*.cpp)
-set(STUFF_PATH ${CMAKE_CURRENT_LIST_DIR}/../..)
-target_sources(${COMPONENT_LIB} PUBLIC ${STUFF_PATH}/ports/esp32/stuff.c)
-target_include_directories(${COMPONENT_LIB} PRIVATE ${STUFF_PATH}/base/include)
-target_link_libraries(${COMPONENT_LIB} PRIVATE base)
--- ports/esp32~/stuff.c
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include <inttypes.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-
-void __stuff_base_time_sleep_us(int64_t microseconds) {
-	uint32_t ticks = microseconds / 1000 / portTICK_PERIOD_MS;
-	vTaskDelay(ticks);
-}
